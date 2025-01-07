420819693fc3224e15e5349906a9ff2bc34d45e1
drax3l <alexjeanpierquispesierra@gmail.com>
2025-01-06 20:37:11 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/mycompany/frm_alexjeanpierquispesierra/Frm_alexjeanpierquispesierra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.frm_alexjeanpierquispesierra;
+
+/**
+ *
+ * @author usuario
+ */
+public class Frm_alexjeanpierquispesierra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
